8bfecf57001c6c80842ff013d4c2b976135f7717
Manas Das <manaspratimdas2004@gmail.com>
2024-06-26 10:17:57 +0530

    Adding logging

--- ConfigServerApp/src/main/java/config/Application.java
+++ ConfigServerApp/src/main/java/config/Application.java
@@ -1,14 +1,21 @@
 package config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+
 @SpringBootApplication
 @EnableConfigServer
 public class Application {
+	
+	private static final Logger logger = LogManager.getLogger(Application.class);
 
   public static void main(String[] args) {
+	  logger.info("Starting CONFIG-SERVER");
     SpringApplication.run(Application.class, args);
   }
 
